97fa121d3f3f7444460759afd013371d998082da
Froopsie <jeremy.n.roos@gmail.com>
2025-04-28 13:57:00 -0400

    Part 2

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'commenter_name' => $this->faker->name(),
+            'comment' => $this->faker->sentence(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Author;
+use Database\Factories\PostFactory;
+use Database\Factories\CommentFactory;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Author::factory()
+            ->count(5)
+            ->has(
+                PostFactory::new()
+                    ->count(3)
+                    ->has(
+                        CommentFactory::new()->count(2)
+                    )
+            )
+            ->create();
     }
 }
